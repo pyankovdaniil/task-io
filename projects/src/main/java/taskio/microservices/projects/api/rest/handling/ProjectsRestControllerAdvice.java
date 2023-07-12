fde9d1daa13d50eaf53c20cb534a93c88dcd7f36
Pyankov Daniil <pyankovdaniil5@gmail.com>
2023-07-12 11:23:48 +0500

    Added custom 404 response message

--- projects/src/main/java/taskio/microservices/projects/api/rest/handling/ProjectsRestControllerAdvice.java
+++ projects/src/main/java/taskio/microservices/projects/api/rest/handling/ProjectsRestControllerAdvice.java
@@ -6,17 +6,34 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import taskio.common.dto.errors.data.DataFieldError;
 import taskio.common.dto.errors.data.InvalidDataResponse;
+import taskio.common.dto.errors.logic.ErrorCode;
 import taskio.common.dto.errors.logic.ErrorEntity;
 import taskio.common.dto.errors.logic.ErrorResponse;
 import taskio.common.exceptions.BaseException;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestControllerAdvice
 public class ProjectsRestControllerAdvice {
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNoHandlerFound(NoHandlerFoundException exception) {
+        return ErrorResponse.builder()
+                .numberOfErrors(1)
+                .errors(List.of(ErrorEntity.builder()
+                        .errorCode(ErrorCode.INVALID_REQUEST_PATH)
+                        .errorDate(new Date(System.currentTimeMillis()))
+                        .errorMessage(exception.getMessage())
+                        .dataCausedError(exception.getRequestURL())
+                        .build()))
+                .build();
+    }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public InvalidDataResponse handleValidationExceptions(MethodArgumentNotValidException ex) {
